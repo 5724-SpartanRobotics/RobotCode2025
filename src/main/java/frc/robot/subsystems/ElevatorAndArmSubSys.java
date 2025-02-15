31d392828c957b025d265f4596686e29014a4542
rickshaw5724 <rick.william.shaw@gmail.com>
2025-02-15 15:22:34 -0500

    build error fix

--- src/main/java/frc/robot/subsystems/ElevatorAndArmSubSys.java
+++ src/main/java/frc/robot/subsystems/ElevatorAndArmSubSys.java
@@ -14,6 +14,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Constant.CanIdConstants;
 import frc.robot.subsystems.Constant.ElevatorAndArmConstants;
