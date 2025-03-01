8dfde68499ea3776e507f1470c180249461c8619
33587 <49316546+33587@users.noreply.github.com>
2025-03-01 08:44:16 -0500

    Changed the Leave to a command and added an end and finish and timer.

--- src/main/java/frc/robot/commands/autos/Leave.java
+++ src/main/java/frc/robot/commands/autos/Leave.java
@@ -1,25 +1,44 @@
 package frc.robot.commands.autos;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
-public class Leave extends SequentialCommandGroup {
+public class Leave extends Command {
+    private DriveTrainSubsystem _DriveTrainSubsystem;
+    Timer _StartTime;
     public Leave(DriveTrainSubsystem driveTrainSubsystem) {
         addRequirements(driveTrainSubsystem);
-        addCommands(
-            new SequentialCommandGroup(
-                new ParallelDeadlineGroup(
-                    new WaitCommand(3),
-                    new RunCommand(() -> {
-                        driveTrainSubsystem.drive(new Translation2d(0.0, 1.0).times(DriveConstants.maxRobotSpeedmps), 0);
-                    })
-                )
-            )
-        );
+        _DriveTrainSubsystem = driveTrainSubsystem;
     }
+    @Override
+    public void execute() {
+        if (_StartTime == null){
+            _StartTime = new Timer();
+            _StartTime.start();
+        }
+        _DriveTrainSubsystem.drive(new Translation2d(-0.3, 0).times(DriveConstants.maxRobotSpeedmps), 0);
+
+    }
+    @Override
+  public void end(boolean interrupted) {
+    _DriveTrainSubsystem.drive(new Translation2d(0.0, 0).times(DriveConstants.maxRobotSpeedmps), 0);
+
+    _StartTime.stop();
+    _StartTime = null;
+  }
+
+  /**
+   * Whether the command has finished. Once a command finishes, the scheduler will call its end()
+   * method and un-schedule it.
+   *
+   * @return whether the command has finished.
+   */
+  public boolean isFinished() {
+    return _StartTime.hasElapsed(3);
+  }
 }
